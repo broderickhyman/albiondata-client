e202816c778c42f3067562f695dd7f0050e1f97f
Regner Blok-Andersen <shadowdf@gmail.com>
2017-05-07 21:17:41 -0700

    First rough pass of the announcer using websockets

---
+++ announcer/registry.go
@@ -0,0 +1,40 @@
+package main
+
+type Registry struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+func NewRegistry() *Registry {
+	return &Registry{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (r *Registry) run() {
+	for {
+		select {
+		case client := <-r.register:
+			r.clients[client] = true
+		case client := <-r.unregister:
+			if _, ok := r.clients[client]; ok {
+				delete(r.clients, client)
+				close(client.send)
+			}
+		case message := <-r.broadcast:
+			for client := range r.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(r.clients, client)
+				}
+			}
+		}
+	}
+}
