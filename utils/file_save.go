e57ce03ef7cdf15fe5ee6639baa0cbd7b46691f4
Regner Blok-Andersen <shadowdf@gmail.com>
2017-07-26 22:52:55 -0700

    * Returning on errors so we don't keep doing things that are clearly broken * Adding new line character to each market order line * Opening file in correct mode so we append to it or create it if needed

--- utils/file_save.go
+++ utils/file_save.go
@@ -11,17 +11,19 @@ func SaveMarketItems(marketItems []string) {
 	date := time.Now().Local().Format("2006-01-02")
 	filename := fmt.Sprintf("marketorder-%v.txt", date)
 
-	f, err := os.OpenFile(filename, os.O_APPEND, 0666)
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
 	if err != nil{
 		log.Printf("Error opeing file for saving: %v", err)
+		return
 	}
 
 	defer f.Close()
 
 	for _, order := range marketItems {
-		_, err := f.WriteString(order)
+		_, err := f.WriteString(fmt.Sprintf("%v\n", order))
 		if err != nil {
 			log.Printf("Error appending to file: %v", err)
+			return
 		}
 	}
 
