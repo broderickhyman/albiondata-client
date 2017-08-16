5dbd5fe6a2883dce959134461fc1d531f2dccdd1
Regner Blok-Andersen <shadowdf@gmail.com>
2017-08-16 12:44:02 -0700

    Testing to see if we can set version from CIRCLE_TAG env var

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y libpcap-dev
 go get -u github.com/golang/dep/cmd/dep
 dep ensure
 
-go build -ldflags "-s -w -X main.version=`git describe --tags`" cmd/albionmarket-client/albionmarket-client.go
+go build -ldflags "-s -w -X main.version=$CIRCLE_TAG" cmd/albionmarket-client/albionmarket-client.go
 
 gzip -9 albionmarket-client
 mv albionmarket-client.gz update-linux-amd64.gz
--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -2,4 +2,5 @@
 
 set -e
 
+echo $CIRCLE_TAG
 go run cmd/albionmarket-client/albionmarket-client.go -o fixtures/refresh-market-02.pcap
