cc4ec012c700f4fd7284415b89bd65cf856b15c6
Rene Jochum <rene@jochums.at>
2017-08-16 16:34:52 +0200

    Extract structs to lib/
    Signed-off-by: Rene Jochum <rene@jochums.at>

---
+++ lib/README.md
@@ -0,0 +1,4 @@
+albiondata-client/lib
+=====================
+
+This package contains the structs needed to work with our json uploads.
---
+++ lib/market.go
@@ -0,0 +1,20 @@
+package lib
+
+// MarketOrder contains an order (offer or request)
+type MarketOrder struct {
+	ID               int    `json:"Id"`
+	ItemID           string `json:"ItemTypeId"`
+	LocationID       int    `json:"LocationId"`
+	QualityLevel     int    `json:"QualityLevel"`
+	EnchantmentLevel int    `json:"EnchantmentLevel"`
+	Price            int    `json:"UnitPriceSilver"`
+	Amount           int    `json:"Amount"`
+	AuctionType      string `json:"AuctionType"`
+	Expires          string `json:"Expires"`
+}
+
+// MarketUpload contains a list of orders and the location where the orders are from
+type MarketUpload struct {
+	Orders     []*MarketOrder
+	LocationID int
+}
