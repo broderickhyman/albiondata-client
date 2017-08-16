bcc1c33e41289b0223bd7d90ae63edc730b0ef97
Regner Blok-Andersen <shadowdf@gmail.com>
2017-08-16 00:25:20 -0700

    Test removing uploader_nats file

--- client/uploader.go
+++ client/uploader.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	nats "github.com/nats-io/go-nats"
 	"github.com/regner/albionmarket-client/log"
 )
 
@@ -14,4 +15,25 @@ type noopUploader struct {
 func (u *noopUploader) sendToIngest(body []byte, queue string) {
 	log.Debugf("Got a noop request to queue: %s", queue)
 	return
+}
+
+type natsUploader struct {
+	url string
+	nc  *nats.Conn
+}
+
+// newNATSUploader create a new NATS uploader :)
+func newNATSUploader(url string) iuploader {
+	nc, _ := nats.Connect(url)
+
+	return &natsUploader{
+		url: url,
+		nc:  nc,
+	}
+}
+
+func (u *natsUploader) sendToIngest(body []byte, queue string) {
+	if err := u.nc.Publish(queue, body); err != nil {
+		log.Errorf("Error while sending ingest to nats with data: %v", err)
+	}
 }
