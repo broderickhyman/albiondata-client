2831148c0d16e427f47d31d9d4b150e8b2f80d7f
Regner Blok-Andersen <shadowdf@gmail.com>
2017-08-16 00:27:17 -0700

    Test adding file back

--- client/uploader.go
+++ client/uploader.go
@@ -1,7 +1,6 @@
 package client
 
 import (
-	nats "github.com/nats-io/go-nats"
 	"github.com/regner/albionmarket-client/log"
 )
 
@@ -15,25 +14,4 @@ type noopUploader struct {
 func (u *noopUploader) sendToIngest(body []byte, queue string) {
 	log.Debugf("Got a noop request to queue: %s", queue)
 	return
-}
-
-type natsUploader struct {
-	url string
-	nc  *nats.Conn
-}
-
-// newNATSUploader create a new NATS uploader :)
-func newNATSUploader(url string) iuploader {
-	nc, _ := nats.Connect(url)
-
-	return &natsUploader{
-		url: url,
-		nc:  nc,
-	}
-}
-
-func (u *natsUploader) sendToIngest(body []byte, queue string) {
-	if err := u.nc.Publish(queue, body); err != nil {
-		log.Errorf("Error while sending ingest to nats with data: %v", err)
-	}
 }
