c249e773263d0c48649d3c7a4a30005610cfe26c
Regner Blok-Andersen <shadowdf@gmail.com>
2017-08-18 18:30:27 -0700

    Set the JSON value for Orders

--- lib/market.go
+++ lib/market.go
@@ -15,5 +15,5 @@ type MarketOrder struct {
 
 // MarketUpload contains a list of orders and the location where the orders are from
 type MarketUpload struct {
-	Orders []*MarketOrder
+	Orders []*MarketOrder `json:"Orders"`
 }
