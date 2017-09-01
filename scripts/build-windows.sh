3a60ee25a3ae208728af0a6f2180b1117590df3a
Rene Jochum <rene@jochums.at>
2017-09-01 22:04:26 +0200

    Add icon to the exe, fixes: #97
    Signed-off-by: Rene Jochum <rene@jochums.at>

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -14,6 +14,9 @@ export CXX=x86_64-w64-mingw32-g++
 export CC=x86_64-w64-mingw32-gcc
 go build -ldflags "-s -w -X main.version=$CIRCLE_TAG" -o albiondata-client.exe -v -x cmd/albiondata-client/albiondata-client.go
 
+# Add icon to the .exe
+wine thirdparty/rcedit/rcedit.exe albiondata-client.exe --set-icon icon/albiondata-client.ico
+
 # Make the NSIS Installer
 cd pkg/nsis
 make nsis
