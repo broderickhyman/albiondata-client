43fc814b1092d264ef26bcb1149357ac83210873
Broderick Hyman <broderickhyman@gmail.com>
2018-02-21 00:41:30 -0500

    Working on osx cross compiling

--- scripts/build-darwin.sh
+++ scripts/build-darwin.sh
@@ -2,17 +2,21 @@
 
 set -eo pipefail
 
-apt-get update && apt-get install -y libpcap-dev
-go get -u github.com/golang/dep/cmd/dep
-dep ensure
+#apt-get update && apt-get install -y libpcap-dev
+#go get -u github.com/golang/dep/cmd/dep
+#dep ensure
 
 export OSXCROSS_NO_INCLUDE_PATH_WARNINGS=1
-export MACOSX_DEPLOYMENT_TARGET=10.6
-export CC=/usr/osxcross/bin/o64-clang
-export CXX=/usr/osxcross/bin/o64-clang++
-export GOOS=darwin
-export GOARCH=amd64 CGO_ENABLED=1
-go build -ldflags "-s -w -X main.version=$CIRCLE_TAG" cmd/albiondata-client/albiondata-client.go
+xgo -ldflags="-s -w -X main.version=$CIRCLE_TAG" --targets=darwin-10.6/amd64 ./cmd/albiondata-client/
 
-gzip -9 albiondata-client
-mv albiondata-client.gz update-darwin-amd64.gz
+TEMP="albiondata-client"
+
+rm -rf ./scripts/$TEMP
+rm -rf ./scripts/update-darwin-amd64.zip
+mkdir ./scripts/$TEMP
+mv albiondata-client-darwin-10.6-amd64 ./scripts/$TEMP/albiondata-client-executable
+cd scripts
+cp run.command ./$TEMP/run.command
+sudo chown -R ${USER}:${USER} ./$TEMP
+chmod a+x ./$TEMP/*
+zip update-darwin-amd64.zip -r ./$TEMP
---
+++ scripts/run.command
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd -- "$(dirname "$BASH_SOURCE")"
+sudo ./albiondata-client-executable
