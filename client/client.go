dc78d3ea2318514c7747a473fa6c6611be69443d
Manuel Laggner <manuel.laggner@gmail.com>
2018-05-01 07:43:40 +0200

    improved logging of start parameters

--- client/client.go
+++ client/client.go
@@ -23,6 +23,13 @@ func (client *Client) Run() {
 	log.Infof("Starting Albion Data Client version %s", version)
 	log.Info("This is a third-party application and is in no way affiliated with Sandbox Interactive or Albion Online.")
 
+	log.Infof("public ingest urls: %s", ConfigGlobal.PublicIngestBaseUrls)
+	log.Infof("private ingest urls: %s", ConfigGlobal.PrivateIngestBaseUrls)
+	log.Infof("disable upload: %v", ConfigGlobal.DisableUpload)
+	log.Infof("offline path: %s", ConfigGlobal.OfflinePath)
+	log.Infof("debug: %v", ConfigGlobal.Debug)
+	log.Infof("listen devices: %s", ConfigGlobal.ListenDevices)
+
 	createDispatcher()
 
 	if ConfigGlobal.Offline {
