e4b81b002bb37fda4f532f1fa8b1ac74f23aa826
Broderick Hyman <broderickhyman@gmail.com>
2020-04-03 23:46:51 -0400

    Queen Patch 9

--- client/albion_state.go
+++ client/albion_state.go
@@ -32,10 +32,14 @@ func (state albionState) IsValidLocation() bool {
 	if state.LocationId < 0 {
 		if state.LocationId == -1 {
 			log.Error("The players location has not yet been set. Please transition zones so the location can be identified.")
-			notification.Push("The players location has not yet been set. Please transition zones so the location can be identified.")
+			if !ConfigGlobal.Debug {
+				notification.Push("The players location has not yet been set. Please transition zones so the location can be identified.")
+			}
 		} else {
 			log.Error("The players location is not valid. Please transition zones so the location can be fixed.")
-			notification.Push("The players location is not valid. Please transition zones so the location can be fixed.")
+			if !ConfigGlobal.Debug {
+				notification.Push("The players location is not valid. Please transition zones so the location can be fixed.")
+			}
 		}
 		return false
 	}
