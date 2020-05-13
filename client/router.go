4906c21fc0e486d0c387b4143a3ea055ac7cf336
Walker de Alencar <walkeralencar@gmail.com>
2020-05-13 10:00:13 -0300

    Format lint fixes

--- client/router.go
+++ client/router.go
@@ -31,7 +31,7 @@ func (r *Router) run() {
 	if ConfigGlobal.RecordPath != "" {
 		file, err := os.Create(ConfigGlobal.RecordPath)
 		if err != nil {
-			log.Errorf("Could not open commands output file ", err)
+			log.Error("Could not open commands output file ", err)
 		} else {
 			encoder = gob.NewEncoder(file)
 		}
@@ -44,7 +44,7 @@ func (r *Router) run() {
 			if file != nil {
 				err := file.Close()
 				if err != nil {
-					log.Errorf("Could not close commands output file ", err)
+					log.Error("Could not close commands output file ", err)
 				}
 			}
 			return
@@ -54,7 +54,7 @@ func (r *Router) run() {
 			if encoder != nil {
 				err := encoder.Encode(command)
 				if err != nil {
-					log.Errorf("Could not encode command ", err)
+					log.Error("Could not encode command ", err)
 				}
 			}
 		}
