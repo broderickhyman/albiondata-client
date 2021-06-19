dbf107faa1a857d4883f5549a189d2ec06f1aa1f
Broderick Hyman <broderickhyman@gmail.com>
2021-06-19 16:31:59 -0400

    Add mac zip for easier usage

--- scripts/build-darwin.sh
+++ scripts/build-darwin.sh
@@ -12,5 +12,36 @@ export GOOS=darwin
 export GOARCH=amd64 CGO_ENABLED=1
 go build -ldflags "-s -w -X main.version=$CIRCLE_TAG" albiondata-client.go
 
+
 gzip -9 albiondata-client
 mv albiondata-client.gz update-darwin-amd64.gz
+
+
+# Creates a zipped folder with a run.command file that runs the client under sudo
+TEMP="albiondata-client"
+ZIPNAME="albiondata-client-amd64-mac.zip"
+rm -rfv ./scripts/$TEMP
+rm -rfv ./$ZIPNAME
+rm -rfv ./scripts/update-darwin-amd64.zip
+mkdir -v ./scripts/$TEMP
+cp -v albiondata-client ./scripts/$TEMP/albiondata-client-executable
+cd scripts
+cp -v run.command ./$TEMP/run.command
+sudo chown -Rv ${USER}:${USER} ./$TEMP
+chmod -v 777 ./$TEMP/*
+zip -v ../$ZIPNAME -r ./"$TEMP"
+
+# In theory the following works to create an app but there was a permissions issue when opening on the mac
+# APP_NAME="Albion Data Client"
+# TEMP="$APP_NAME".app
+# ZIPNAME="albiondata-client-amd64-mac.zip"
+
+# rm -rfv ./scripts/"$TEMP"
+# rm -rfv ./scripts/"$ZIPNAME"
+# mkdir -pv ./scripts/"$TEMP"/Contents/MacOS
+# cp -v albiondata-client-darwin-10.6-amd64 ./scripts/"$TEMP"/Contents/MacOS/"$APP_NAME"
+# sudo chown -Rv ${USER}:${USER} ./scripts/"$TEMP"
+# chmod -v 777 ./scripts/"$TEMP"/*
+
+# cd scripts
+# zip -v ../$ZIPNAME -r ./"$TEMP"
