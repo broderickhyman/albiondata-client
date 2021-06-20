e213df5ac5578488671de42d9eb51427f51e9b98
Walker de Alencar <walkeralencar@gmail.com>
2021-06-20 17:22:58 -0300

    start refactoring configurations to client/config.go

--- client/config.go
+++ client/config.go
@@ -1,5 +1,13 @@
 package client
 
+import (
+	"flag"
+	"strconv"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
 type config struct {
 	AllowedWSHosts                 []string
 	Debug                          bool
@@ -27,4 +35,170 @@ type config struct {
 //ConfigGlobal global config data
 var ConfigGlobal = &config{
 	LogLevel: "INFO",
+}
+
+func ConfigSetup() {
+	// Setup the config file and parse values
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+
+	// if we cannot find the configuration file, set Websockets to false
+	if err != nil {
+		viper.Set("EnableWebsockets", false)
+	}
+
+	ConfigGlobal.EnableWebsockets = viper.GetBool("EnableWebsockets")
+	ConfigGlobal.AllowedWSHosts = viper.GetStringSlice("AllowedWebsocketHosts")
+
+	flag.BoolVar(
+		&ConfigGlobal.Debug,
+		"debug",
+		false,
+		"Enable debug logging.",
+	)
+
+	flag.StringVar(
+		&ConfigGlobal.DebugEventsString,
+		"events",
+		"",
+		"Whitelist of event IDs to output messages when debugging. Comma separated.",
+	)
+
+	flag.StringVar(
+		&ConfigGlobal.DebugEventsBlacklistString,
+		"events-ignore",
+		"",
+		"Blacklist of event IDs to hide messages when debugging. Comma separated.",
+	)
+
+	flag.StringVar(
+		&ConfigGlobal.DebugOperationsString,
+		"operations",
+		"",
+		"Whitelist of operation IDs to output messages when debugging. Comma separated.",
+	)
+
+	flag.StringVar(
+		&ConfigGlobal.DebugOperationsBlacklistString,
+		"operations-ignore",
+		"",
+		"Blacklist of operation IDs to hide messages when debugging. Comma separated.",
+	)
+
+	flag.BoolVar(
+		&ConfigGlobal.DebugIgnoreDecodingErrors,
+		"ignore-decode-errors",
+		false,
+		"Ignore the decoding errors when debugging",
+	)
+
+	flag.BoolVar(
+		&ConfigGlobal.NoCPULimit,
+		"no-limit",
+		false,
+		"Use all available CPU cores",
+	)
+
+}
+
+func (config *config) setupCommonFlags() {
+	flag.BoolVar(
+		&ConfigGlobal.DisableUpload,
+		"d",
+		false,
+		"If specified no attempts will be made to upload data to remote server.",
+	)
+
+	flag.StringVar(
+		&ConfigGlobal.ListenDevices,
+		"l",
+		"",
+		"Listen on this comma separated devices instead of all available",
+	)
+
+	flag.BoolVar(
+		&ConfigGlobal.LogToFile,
+		"output-file",
+		false,
+		"Enable logging to file.",
+	)
+
+	flag.StringVar(
+		&ConfigGlobal.OfflinePath,
+		"o",
+		"",
+		"Parses a local file instead of checking albion ports.",
+	)
+
+	flag.BoolVar(
+		&ConfigGlobal.Minimize,
+		"minimize",
+		false,
+		"Automatically minimize the window.",
+	)
+
+	flag.StringVar(
+		&ConfigGlobal.PublicIngestBaseUrls,
+		"i",
+		"http+pow://www.albion-online-data.com:4223",
+		"Base URL to send PUBLIC data to, can be 'nats://', 'http://' or 'noop' and can have multiple uploaders. Comma separated.",
+	)
+
+	flag.StringVar(
+		&ConfigGlobal.PrivateIngestBaseUrls,
+		"p",
+		"",
+		"Base URL to send PRIVATE data to, can be 'nats://', 'http://' or 'noop' and can have multiple uploaders. Comma separated.",
+	)
+
+	flag.StringVar(
+		&ConfigGlobal.RecordPath,
+		"record",
+		"",
+		"Enable recording commands to a file for debugging later.",
+	)
+}
+
+func (config *config) SetupDebugEvents() {
+	config.DebugEvents = make(map[int]bool)
+	if config.DebugEventsString != "" {
+		for _, event := range strings.Split(config.DebugEventsString, ",") {
+			number, err := strconv.Atoi(event)
+			if err == nil {
+				config.DebugEvents[number] = true
+			}
+		}
+	}
+	if config.DebugEventsBlacklistString != "" {
+		for _, event := range strings.Split(config.DebugEventsBlacklistString, ",") {
+			number, err := strconv.Atoi(event)
+			if err == nil {
+				config.DebugEvents[number] = false
+			}
+		}
+	}
+
+}
+
+func (config *config) SetupDebugOperations() {
+	config.DebugOperations = make(map[int]bool)
+	if config.DebugOperationsString != "" {
+		for _, operation := range strings.Split(config.DebugOperationsString, ",") {
+			number, err := strconv.Atoi(operation)
+			if err == nil {
+				config.DebugOperations[number] = true
+			}
+		}
+	}
+
+	if config.DebugOperationsBlacklistString != "" {
+		for _, operation := range strings.Split(config.DebugOperationsBlacklistString, ",") {
+			number, err := strconv.Atoi(operation)
+			if err == nil {
+				config.DebugOperations[number] = false
+			}
+		}
+	}
+
 }
